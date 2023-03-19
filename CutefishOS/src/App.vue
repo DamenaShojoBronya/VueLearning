bd9e7a74a2d0f2fbe19b05b8e840a3239f534dbe
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2023-03-20 01:58:44 +0800

    test
    test

---
+++ CutefishOS/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ApplePage></ApplePage>
+
+</template>
+
+<script>
+import ApplePage from './components/ApplePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ApplePage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ CutefishOS/src/components/ApplePage.vue
@@ -0,0 +1,481 @@
+<template>
+  <div class="home">
+    <div class="header">
+      <div class="container">
+        <nav class="navbar navbar-light navbar-expand-md">
+          <a href="/web/20220430003034mp_/https://en.cutefishos.com/" aria-current="page"
+            class="navbar-brand router-link-exact-active router-link-active" target="_self">
+            <img src="../assets/logo.png"
+              alt="Kitten" class="d-inline-block align-center" style="width: 120px; margin-right: 5px; margin-top: -3px;">
+          </a>
+
+          <div id="nav-collapse" class="navbar-collapse collapse"
+            style="display: none; justify-content: flex-end;">
+            <ul class="navbar-nav mr">
+              <li class="nav-item"><a href="#" target="_self"
+                  class="nav-link active">
+              <li class="navbar-text">Home</li></a></li>
+              <li class="nav-item"><a href="#" target="_self" class="nav-link">
+              <li class="navbar-text">Download</li></a></li>
+              <li class="nav-item"><a href="#" target="_self" class="nav-link">
+              <li class="navbar-text">About</li></a></li>
+              <li class="nav-item"><a href="#" target="_self" class="nav-link">
+              <li class="navbar-text">Forum</li></a></li>
+              <li class="nav-item"><a href="#" target="_self" class="nav-link">
+              <li class="navbar-text">简体中文</li></a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <div class="home-main">
+      <div class="header-content">
+        <div class="container"><img
+            src="../assets/img/home.png" class="img-fluid">
+          <div class="description">
+            <h1 style="padding-bottom: 5px;">Make a better Desktop OS</h1>
+            <h2 style="padding-bottom: 20px;">Focus on simplicity, beauty and practicality</h2><button
+             type="button" class="btn btn btn-outline-primary rounded-pill">Download 0.8 Beta</button>
+          </div>
+        </div>
+      </div>
+      <div style="background: rgb(245, 248, 253);">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5"><img
+                  src="../assets/img/color-wheel.svg" class="img-fluid">
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2>Better user experience</h2>
+                <p>Simple and exquisite design, use the most suitable design to enhance user
+                  experience.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div style="background: rgb(255, 255, 255);">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-1">
+              <div class="p-5">
+                <h2>CutefishOS Applications</h2>
+                <p>We have developed a series of cutefish OS applications to ensure that users
+                  experience a unified UI / UX experience in their daily use.</p>
+              </div>
+            </div>
+            <div class="col-lg-6 order-lg-2">
+              <div class="p-5"><img
+                  src="../assets/img/apps.svg" class="img-fluid"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style="background-image: linear-gradient(103.3deg, rgb(252, 225, 208) 30%, rgb(255, 173, 214) 55.7%, rgb(162, 186, 245) 81.8%);">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5"><img
+                  src="../assets/img/desktop.png" class="img-fluid"></div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2>Cutefish Desktop</h2>
+                <p>Cutefish desktop environment is an efficient, beautiful, and modern desktop
+                  environment built using Qt Quick and some KDE basic frameworks.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style="background-image: radial-gradient(circle at 10% 20%, rgb(215, 223, 252) 0%, rgb(255, 255, 255) 0%, rgb(215, 223, 252) 84%);">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-1">
+              <div class="p-5">
+                <h2>Global Menu</h2>
+                <p>Cutefishos has a global menu at the top. It is a collection of all functions of an
+                  application, which is very convenient for the integration of apps functions with the system, and can
+                  save some screen space.</p>
+              </div>
+            </div>
+            <div class="col-lg-6 order-lg-2">
+              <div class="p-5"><img
+                  src="../assets/img/menubar.png" class="img-fluid"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="black-background">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6">
+              <div class="p-5 order-lg-2"><img
+                  src="../assets/img/security.svg" class="img-fluid">
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="p-5">
+                <h2>Security and stability</h2>
+                <p>CutefishOS is based on the open source Linux kernel. Linux is more secure than other
+                  operating systems. It has developers from all over the world to review its code and ensure that there
+                  are no bugs and backdoors. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="green-background">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 order-lg-1">
+              <div class="p-5">
+                <h2>Open source</h2>
+                <p>CutefishOS is an open source project, anyone can understand how it works and
+                  participate in collaboration.</p>
+              </div>
+            </div>
+            <div class="col-lg-6 order-lg-2">
+              <div class="p-5"><img
+                  src="../assets/img/opensource.svg" class="img-fluid">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="footer-des container">
+        <div class="row">
+          <div class="col-lg-4">
+            <h3>Links</h3>
+            <ul>
+              <li><a href="/web/20220430003034mp_/https://en.cutefishos.com/about"
+                  class="">About</a></li>
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://cutefishos.github.io/docs"
+                  target="_blank">Docs</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4">
+            <h3>Friends</h3>
+            <ul>
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://jingos.com/" target="_blank">JingOS</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4">
+            <h3>Community</h3>
+            <ul>
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://forum.cutefishos.com/"
+                  target="_blank">Forum</a></li><!---->
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://github.com/cutefishos"
+                  target="_blank">Github</a></li>
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://twitter.com/cutefishos"
+                  target="_blank">Twitter</a></li>
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://discord.gg/mhUVu4dPH8"
+                  target="_blank">Discord</a></li>
+              <li><a
+                  href="https://web.archive.org/web/20220425222753/https://t.me/CutefishOS" target="_blank">Telegram</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="copyright">
+        <p align="center">Copyright © 2021 - 2022 CutefishOS Team</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ApplePage',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.container{
+  margin:0 auto;
+}
+h1{
+  font-size:2.5rem;
+}
+h2 {
+    font-size: 2rem;
+}
+h3 {
+    font-size: 1.75rem;
+}
+.h1, .h2, .h3, .h4, .h5, .h6 {
+    margin-bottom: 0.5rem;
+    font-weight: 500;
+    line-height: 1.2;
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+}
+h2 {
+    display: block;
+    font-size: 1.8em;
+    margin-block-start: 0.83em;
+    margin-block-end: 0.83em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+    line-height: 0rem;
+    text-align: left;
+}
+.header-content .container .description h2{
+  text-align: center;
+}
+
+.description {
+    padding-top: 20px;
+}
+p {
+    margin-top: 0;
+    margin-bottom: 1rem;
+    line-height: 1.5rem;
+    text-align: left;
+}
+p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+}
+:after, :before {
+    box-sizing: border-box;
+}
+li {
+    display: list-item;
+    text-align: -webkit-match-parent;
+}
+a {
+    color: #007bff;
+    text-decoration: none;
+    background-color: transparent;
+}
+a:-webkit-any-link {
+    color: -webkit-link;
+    cursor: pointer;
+}
+a:visited { 
+font-size: 12px; 
+color: #000000; 
+text-decoration: none; 
+}
+img, svg {
+    vertical-align: middle;
+}
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  z-index: 99999;
+}
+.home-main .header-content{
+    padding-bottom: 40px;
+    padding-top: 100px;
+    text-align: center;
+}
+.navbar-brand {
+    display: inline-block;
+    padding-top: 0.3125rem;
+    padding-bottom: 0.3125rem;
+    margin-right: 1rem;
+    font-size: 1.25rem;
+    line-height: inherit;
+    white-space: nowrap;
+}
+.navbar-nav {
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: column;
+    padding-left: 0;
+    margin-bottom: 0;
+    list-style: none;
+
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+}
+
+.navbar-expand-md .navbar-collapse {
+    display: flex!important;
+    flex-basis: auto;
+}
+.navbar-expand-md {
+    margin-left:3%;
+    margin-right:3%;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-end;
+    flex-wrap: nowrap;
+    flex-direction: row;
+    align-content: center;
+    align-items: flex-end;
+}
+.navbar-collapse {
+    flex-basis: 100%;
+    flex-grow: 1;
+    align-items: center;
+}
+.navbar-text {
+    display: inline-block;
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+}
+.navbar-expand-md .navbar-nav .nav-link {
+    padding-right: 0.5rem;
+    padding-left: 0.5rem;
+}
+.container {
+  max-width: 1140px;
+}
+.img-fluid{
+  width: 100%;
+  height: 100%;
+}
+.header-content .container .img-fluid{
+  width: 50%;
+}
+.header-content .btn{
+    width: 150px;
+}
+.btn:not(:disabled):not(.disabled) {
+    cursor: pointer;
+}
+.rounded-pill {
+    border-radius: 50rem!important;
+}
+.btn11 {
+    margin:0;
+    display: inline-block;
+    font-weight: 400;
+    color: #212529;
+    text-align: center;
+    vertical-align: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid transparent;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+.btn {
+    margin:0;
+    display: inline-block;
+    color: #007bff;
+    text-align: center;
+    vertical-align: middle;
+    user-select: none;
+    background-color: transparent;
+    border: 1px solid #007bff;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    border-radius: 0.25rem;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+.btn-outline-primary:hover {
+    color: #fff;
+    background-color: #007bff;
+    border-color: #007bff;
+}
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -15px;
+    margin-left: -15px;
+}
+.col-lg-6 {
+    display: flex;
+    flex: 0 0 50%;
+    max-width: 50%;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: nowrap;
+    flex-direction: row;
+}
+.p-5 {
+    padding: 3rem!important;
+}
+.home-main .black-background {
+    background-image: linear-gradient(76.3deg,#527491 12.6%,#456783 82.8%);
+}
+
+.home-main .black-background h2, .home-main .black-background p,
+.home-main .green-background h2, .home-main .green-background p {
+    color: #fff;
+}
+.home-main .green-background {
+    background-image: radial-gradient(circle farthest-corner at 10% 20%,#4390e9 0,#4ee2c6 90.1%);
+}
+.footer{
+    background-color: #f3f3f7;
+    color: #5d5d5d;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    font-size: 14px;
+    text-align: center;
+    padding-top: 40px;
+    flex-direction: column;
+    flex-wrap: nowrap;
+}
+.footer .footer-des {
+    margin-bottom: 60px;
+    text-align: left;
+}
+.footer .footer-des li, .footer .footer-des ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+}
+html,body{
+  margin:0px;
+}
+.col-lg-4 {
+    flex: 0 0 33.333333%;
+    max-width: 33.333333%;
+}
+.footer .footer-des li {
+    margin-top: 10px;
+}
+.footer .footer-des ul>li>a {
+    color: #5d5d5d;
+}
+.footer .copyright{
+    padding: 20px 0;
+    background-color: #f3f3f7;
+    color: #5d5d5d;
+}
+.footer .copyright p {
+    margin-bottom: unset;
+    text-align: center;
+}
+</style>

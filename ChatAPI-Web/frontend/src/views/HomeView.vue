515fc2c406c56e2045edacc13bd318405b64e6fa
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2023-03-27 23:33:28 +0800

    update
    update

--- ChatAPI-Web/frontend/src/views/HomeView.vue
+++ ChatAPI-Web/frontend/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <template>
-  <el-container>
+  <el-container >
     <el-carousel class="home" :interval="4000" type="card" height="200px">
       <el-carousel-item v-for="item in 6" :key="item">
         <h3 text="2xl" justify="center">{{ item }}</h3>
       </el-carousel-item>
     </el-carousel>
   </el-container>
+  <div class="ui-block ui-block--listing-mixed theme-dark-gray">
+  </div>
 </template>
 
 <script>
@@ -17,14 +19,6 @@ export default {
   }
 }
 </script>
-<!--  <div data-v-9ea40744="" class="el-carousel el-carousel--horizontal el-carousel--card">
-<div class="el-carousel__container" style="height: 200px;">
-<button type="button" class="el-carousel__arrow el-carousel__arrow--left" style="display: none;"><i class="el-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M609.408 149.376 277.76 489.6a32 32 0 0 0 0 44.672l331.648 340.352a29.12 29.12 0 0 0 41.728 0 30.592 30.592 0 0 0 0-42.752L339.264 511.936l311.872-319.872a30.592 30.592 0 0 0 0-42.688 29.12 29.12 0 0 0-41.728 0z"></path></svg></i></button>
-<button type="button" class="el-carousel__arrow el-carousel__arrow--right" style="display: none;"><i class="el-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path fill="currentColor" d="M340.864 149.312a30.592 30.592 0 0 0 0 42.752L652.736 512 340.864 831.872a30.592 30.592 0 0 0 0 42.752 29.12 29.12 0 0 0 41.728 0L714.24 534.336a32 32 0 0 0 0-44.672L382.592 149.376a29.12 29.12 0 0 0-41.728 0z"></path></svg></i></button>
-<div data-v-9ea40744="" class="el-carousel__item el-carousel__item--card" style="transform: translateX(218.31px) scale(0.83);">
-<div class="el-carousel__mask" style=""></div>
-<h3 data-v-9ea40744="" text="2xl" justify="center">1</h3></div>
-<div data-v-9ea40744="" class="el-carousel__item el-carousel__item--card" style="transform: translateX(218.31px) scale(0.83);"><div class="el-carousel__mask" style=""></div> -->
 
 <style scoped>
 .el-container{

1ae0b52a13bc17c37c548cc3b325ca64739a9f92
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2023-03-29 23:56:14 +0800

    update
    update

--- ChatAPI-Web/frontend/src/views/AboutView.vue
+++ ChatAPI-Web/frontend/src/views/AboutView.vue
@@ -1,64 +1,74 @@
 <template>
-  <div class="content-1-mainbox">
-    <div class="banner">
-      <div class="banner-icon-wrap">
-        <img src="../assets/图标-风控能力-文本鉴黄.png">
+  <div class="content-1">
+    <div class="content-1-home">
+      <div class="content-1-tittle-wrap">
+        <h3 class="background">RISK CONTROL CAPABILITY</h3>
+        <div class="content-1-tittle">
+          <p>风控能力</p>
+          <img src="assets/标题装饰-1.png">
+        </div>
       </div>
-      <div class="banner-text-wrap">
-        <h3>文本鉴黄</h3>
-        <p>色情传播、性行为、性器官描写、低俗段子、低俗隐晦等</p>
-      </div>
-    </div>
-    <div class="banner">
-      <div class="banner-icon-wrap">
-        <img src="../assets/图标-风控能力-涉政暴恐.png">
-      </div>
-      <div class="banner-text-wrap">
-        <h3>涉政暴恐</h3>
-        <p>对国家、政府官员和英雄烈士的不当言论；恐怖组织和恐怖事件宣传相关信息</p>
-      </div>
-    </div>
-    <div class="banner">
-      <div class="banner-icon-wrap">
-        <img src="../assets/图标-风控能力-违禁检测.png">
-      </div>
-      <div class="banner-text-wrap">
-        <h3>违禁检测</h3>
-        <p>赌博、毒品、非法传教、贩卖野生动物、非法办证考证等非法违禁信息 </p>
-      </div>
-    </div>
-    <div class="banner">
-      <div class="banner-icon-wrap">
-        <img src="../assets/图标-风控能力-篡改检测.png">
-      </div>
-      <div class="banner-text-wrap">
-        <h3>篡改检测</h3>
-        <p>对网站内容主动监测，及时扼制因恶意篡改而造成内容违规风险 </p>
-      </div>
-    </div>
-    <div class="banner">
-      <div class="banner-icon-wrap">
-        <img src="../assets/图标-风控能力-挂马识别.png">
-      </div>
-      <div class="banner-text-wrap">
-        <h3>挂马识别</h3>
-        <p>基于多种模型融合技术，及时发现页面挂马，并第一时间进行预警通知。</p>
-      </div>
-    </div>
-    <div class="banner">
-      <div class="banner-icon-wrap">
-        <img src="../assets/图标-风控能力-暗链识别.png">
-      </div>
-      <div class="banner-text-wrap">
-        <h3>暗链识别</h3>
-        <p>全面防范，让暗链、黑链、死链无所遁形</p>
+      <div class="content-1-mainbox">
+        <div class="banner">
+          <div class="banner-icon-wrap">
+            <img src="assets/图标-风控能力-文本鉴黄.png">
+          </div>
+          <div class="banner-text-wrap">
+            <h3>文本鉴黄</h3>
+            <p>色情传播、性行为、性器官描写、低俗段子、低俗隐晦等</p>
+          </div>
+        </div>
+        <div class="banner">
+          <div class="banner-icon-wrap">
+            <img src="assets/图标-风控能力-涉政暴恐.png">
+          </div>
+          <div class="banner-text-wrap">
+            <h3>涉政暴恐</h3>
+            <p>对国家、政府官员和英雄烈士的不当言论；恐怖组织和恐怖事件宣传相关信息</p>
+          </div>
+        </div>
+        <div class="banner">
+          <div class="banner-icon-wrap">
+            <img src="assets/图标-风控能力-违禁检测.png">
+          </div>
+          <div class="banner-text-wrap">
+            <h3>违禁检测</h3>
+            <p>赌博、毒品、非法传教、贩卖野生动物、非法办证考证等非法违禁信息 </p>
+          </div>
+        </div>
+        <div class="banner">
+          <div class="banner-icon-wrap">
+            <img src="assets/图标-风控能力-篡改检测.png">
+          </div>
+          <div class="banner-text-wrap">
+            <h3>篡改检测</h3>
+            <p>对网站内容主动监测，及时扼制因恶意篡改而造成内容违规风险 </p>
+          </div>
+        </div>
+        <div class="banner">
+          <div class="banner-icon-wrap">
+            <img src="assets/图标-风控能力-挂马识别.png">
+          </div>
+          <div class="banner-text-wrap">
+            <h3>挂马识别</h3>
+            <p>基于多种模型融合技术，及时发现页面挂马，并第一时间进行预警通知。</p>
+          </div>
+        </div>
+        <div class="banner">
+          <div class="banner-icon-wrap">
+            <img src="assets/图标-风控能力-暗链识别.png">
+          </div>
+          <div class="banner-text-wrap">
+            <h3>暗链识别</h3>
+            <p>全面防范，让暗链、黑链、死链无所遁形</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .content-1-mainbox {
   height: 398px;
   display: flex;
@@ -113,4 +123,5 @@
   font-size: 14px;
   color: black;
   line-height: 22px;
-}</style>
+}
+</style>

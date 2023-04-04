36fc75c682aa5bdd1b9a9cc0b111ccf8d2829026
DamenaShojoBronya <43756924+DamenaShojoBronya@users.noreply.github.com>
2023-04-04 22:33:45 +0800

    update
    update

--- ChatAPI-Web/frontend/src/views/Products/LecturehallApply.vue
+++ ChatAPI-Web/frontend/src/views/Products/LecturehallApply.vue
@@ -1,12 +1,18 @@
 <template>
     <el-steps :active="1" finish-status="success" simple style="margin-top: 20px">
-        <el-step title="Step 1" />
-        <el-step title="Step 2" />
-        <el-step title="Step 3" />
+      <el-step title="Step 1" />
+      <el-step title="Step 2" />
+      <el-step title="Step 3" />
     </el-steps>
-</template>
+  </template>
   
-<script lang="ts" setup>
-// import { Edit, Picture, UploadFilled } from '@element-plus/icons-vue'
-</script>
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    setup() {
+      // 这里添加需要的响应式数据和方法
+    },
+  });
+  </script>
   
--- ChatAPI-Web/frontend/src/views/Products/SubmitForm.vue
+++ ChatAPI-Web/frontend/src/views/Products/SubmitForm.vue
@@ -1,13 +1,6 @@
 <template>
-  <el-form
-    ref="ruleFormRef"
-    :model="ruleForm"
-    :rules="rules"
-    label-width="120px"
-    class="demo-ruleForm"
-    :size="formSize"
-    status-icon
-  >
+  <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm" :size="formSize"
+    status-icon>
     <el-form-item label="报修地点" prop="location">
       <el-input v-model="ruleForm.location" />
     </el-form-item>
@@ -29,47 +22,61 @@
   </el-form>
 </template>
 
-<script lang="ts" setup>
-import { reactive, ref } from 'vue'
-import type { FormInstance, FormRules } from 'element-plus'
+<script lang="ts">
+import { defineComponent, reactive, ref } from 'vue';
+import type { FormInstance, FormRules } from 'element-plus';
 
-const formSize = ref('default')
-const ruleFormRef = ref<FormInstance>()
-const ruleForm = reactive({
-  location: '',
-  description: '',
-  reporter: '',
-  contact: '',
-})
+export default defineComponent({
+  setup() {
+    const formSize = ref('default');
+    const ruleFormRef = ref<FormInstance>();
+    const ruleForm = reactive({
+      location: '',
+      description: '',
+      reporter: '',
+      contact: '',
+    });
 
-const rules = reactive<FormRules>({
-  location: [
-    { required: true, message: '请输入报修地点', trigger: 'blur' },
-  ],
-  description: [
-    { required: true, message: '请输入问题描述', trigger: 'blur' },
-  ],
-  reporter: [
-    { required: true, message: '请输入报修人员', trigger: 'blur' },
-  ],
-  contact: [
-    { required: true, message: '请输入联系方式', trigger: 'blur' },
-  ],
-})
+    const rules = reactive<FormRules>({
+      location: [
+        { required: true, message: '请输入报修地点', trigger: 'blur' },
+      ],
+      description: [
+        { required: true, message: '请输入问题描述', trigger: 'blur' },
+      ],
+      reporter: [
+        { required: true, message: '请输入报修人员', trigger: 'blur' },
+      ],
+      contact: [
+        { required: true, message: '请输入联系方式', trigger: 'blur' },
+      ],
+    });
 
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      console.log('submit!')
-    } else {
-      console.log('error submit!', fields)
-    }
-  })
-}
+    const submitForm = async (formEl: FormInstance | undefined) => {
+      if (!formEl) return;
+      await formEl.validate((valid, fields) => {
+        if (valid) {
+          console.log('submit!');
+        } else {
+          console.log('error submit!', fields);
+        }
+      });
+    };
 
-const resetForm = (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  formEl.resetFields()
-}
+    const resetForm = (formEl: FormInstance | undefined) => {
+      if (!formEl) return;
+      formEl.resetFields();
+    };
+
+    return {
+      formSize,
+      ruleFormRef,
+      ruleForm,
+      rules,
+      submitForm,
+      resetForm,
+    };
+  },
+});
 </script>
+
